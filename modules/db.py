fc2f5d59b3e9f03bdfa3979b1f0bf8bfed1949de
lijian <lijian8931@163.com>
2017-09-12 13:16:13 +0800

    add line

--- modules/db.py
+++ modules/db.py
@@ -40,7 +40,7 @@ class DB:
         if type(dt_list) != list:
             raise Exception("select_content_by_dt_list: dt must be list")
         query_result = self.session.query(Note).filter(Note.date.in_(dt_list), Note.email == email).all()
-        result = ["<div>------" + str(r.get_date()) + "------</div>" + str(r.get_content()) for r in query_result]
+        result = ["<div>------" + str(r.get_date()) + "------</div>" + str(r.get_content()) + "<hr />" for r in query_result]
         return result
 
     def add_note(self, email, content, dt):
